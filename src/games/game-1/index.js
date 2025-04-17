7ef407dcdd4511a4372c385316346cf3a91860d1
Bruno Becoski <brunobecoski@gmail.com>
2025-04-16 22:15:27 -0300

    Atualiza o componente Game 1

--- src/games/game-1/index.js
+++ src/games/game-1/index.js
@@ -1,5 +1,24 @@
+import { createButtonMarket } from './components/buttonMarket'
+
+import marketImg from '../../assets/market.png'
+import goldImg from '../../assets/gold.png'
+import woodImg from '../../assets/wood.png'
+import foodImg from '../../assets/food.png'
+import stoneImg from '../../assets/stone.png'
+
 export function createGame1() {
   const section__element = document.createElement('section')
+  const img__element = document.createElement('img')
+  const img__element_gold = document.createElement('img')
+  const img__element_wood = document.createElement('img')
+  const img__element_food = document.createElement('img')
+  const img__element_stone = document.createElement('img')
+
+  img__element.src = marketImg
+  img__element_gold.src = goldImg
+  img__element_wood.src = woodImg
+  img__element_food.src = foodImg
+  img__element_stone.src = stoneImg
   
   section__element.innerHTML = `
     <div class="container">
@@ -10,6 +29,41 @@ export function createGame1() {
       </div>
     </div>
   `
+  const button_gold__element = createButtonMarket({
+    price: '100',
+    img__element: img__element_gold,
+    handleClick: () => console.log('vender ouro'),
+    title: 'Vender'
+  })
+
+  const button_wood__element = createButtonMarket({
+    price: '100',
+    img__element: img__element_wood,
+    handleClick: () => console.log('vender madeira'),
+    title: 'Vender'
+  })
+
+  const button_food__element = createButtonMarket({
+    price: '100',
+    img__element: img__element_food,
+    handleClick: () => console.log('vender comida'),
+    title: 'Vender'
+  })
+
+  const button_stone__element = createButtonMarket({
+    price: '100',
+    img__element: img__element_stone,
+    handleClick: () => console.log('vender ouro'),
+    title: 'Vender'
+  })
+
+  section__element.append(
+    img__element,
+    button_gold__element,
+    button_wood__element,
+    button_food__element,
+    button_stone__element,
+  )
 
   section__element.querySelector('button').onclick = () => handlePlay()
 
