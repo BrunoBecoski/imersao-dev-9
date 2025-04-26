70210034aabf6d1b576da75e9db7ea490bf6355a
Bruno Becoski <brunobecoski@gmail.com>
2025-04-26 20:13:00 -0300

    Atualiza o componente Game 1

--- src/games/game-1/index.js
+++ src/games/game-1/index.js
@@ -6,45 +6,137 @@ import stoneImg from '../../assets/stone.png'
 
 import './styles.css'
 
-let wood = 100
-let food = 100
-let stone = 100
-
-let wood_exchange = 100
-let food_exchange = 115
-let stone_exchange = 130
-
 function createGold() {
-  let value = 1000
-
-  function amount() {
-    return value
-  }
+  let quantity = 1500
 
   function increase() {
-    value = value + 100
-
+    quantity = quantity + 100
     render()
   }
 
   function decrease() {
-    value = value - 100
+    quantity = quantity - 100
+    render()
+  }
+
+  function render() {
+    document.getElementById('gold_resource').innerText = quantity
+  }
+
+  return {
+    quantity: () => quantity,
+    increase, 
+    decrease,
+  }
+}
+
+function createWood() {
+  let quantity = 500
+  let price = 100
+
+  function increase() {
+    quantity = quantity + price
+    if (price < 220) {
+      price = price + 5
+    }
+    render()
+  }
+
+  function decrease() {
+    quantity = quantity - price
+    if (price > 20) {
+      price = price - 5
+    }
 
     render()
   }
 
   function render() {
-    document.querySelector('#game_1 .resources #gold').innerText = value
+    document.getElementById('wood_resource').innerText = quantity
+    document.getElementById('buy_wood_price').innerText = price
+    document.getElementById('sell_wood_price').innerText = price
   }
 
   return {
-    amount,
+    quantity: () => quantity,
+    price: () => price,
+    increase,
+    decrease,
+  }
+}
+
+function createFood() {
+  let quantity = 500
+  let price = 115
+
+  function increase() {
+    quantity = quantity + price
+    if (price < 220) {
+      price = price + 5
+    }
+    render()
+  }
+
+  function decrease() {
+    quantity = quantity - price
+    if (price > 20) {
+      price = price - 5
+    }
+    render()
+  }
+
+  function render() {
+    document.getElementById('food_resource').innerText = quantity
+    document.getElementById('buy_food_price').innerText = price
+    document.getElementById('sell_food_price').innerText = price
+  }
+
+  return {
+    quantity: () => quantity,
+    price: () => price,
+    increase,
+    decrease,
+  }
+}
+
+function createStone() {
+  let quantity = 500
+  let price = 130
+
+  function increase() {
+    quantity = quantity + price
+    if (price < 220) {
+      price = price + 5
+    }
+    render()
+  }
+
+  function decrease() {
+    quantity = quantity - price
+    if (price > 20) {
+      price = price - 5
+    }
+    render()
+  }
+
+  function render() {
+    document.getElementById('stone_resource').innerText = quantity
+    document.getElementById('buy_stone_price').innerText = price
+    document.getElementById('sell_stone_price').innerText = price
+  }
+
+  return {
+    quantity: () => quantity,
+    price: () => price,
     increase,
     decrease,
   }
 }
 
 const gold = createGold()
+const wood = createWood()
+const food = createFood()
+const stone = createStone()
 
 export function createGame1() {
   const section__element = document.createElement('section')
@@ -52,25 +144,10 @@ export function createGame1() {
   section__element.innerHTML = `
     <div id="game_1">
       <div class="resources">
-        <div>
-          <img src=${goldImg} />
-          <span id="gold">${gold.amount()}</span>
-        </div>
-
-        <div>
-          <img src=${woodImg} />
-          <span id="wood">${wood}</span>
-        </div>
-
-        <div>
-          <img src=${foodImg} />
-          <span id="food">${food}</span>
-        </div>
-
-        <div>
-          <img src=${stoneImg} />
-          <span id="stone">${stone}</span>
-        </div>
+        <div><img src=${goldImg} /><span id="gold_resource">${gold.quantity()}</span></div>
+        <div><img src=${woodImg} /><span id="wood_resource">${wood.quantity()}</span></div>
+        <div><img src=${foodImg} /><span id="food_resource">${food.quantity()}</span></div>
+        <div><img src=${stoneImg} /><span id="stone_resource">${stone.quantity()}</span></div>
       </div>
 
       <div class="market">
@@ -78,137 +155,67 @@ export function createGame1() {
 
         <div class="exchange">
           <div class="sell">
-            <button id="wood" title="Madeira"><span>${wood_exchange}</span> <img src=${woodImg}  /> Vender</button>
-            <button id="food" title="Comida"><span>${food_exchange}</span> <img src=${foodImg} /> Vender</button>
-            <button id="stone" title="Pedra"><span>${stone_exchange}</span> <img src=${stoneImg} /> Vender</button>
+            <button id="sell_wood" title="Madeira"><span id="sell_wood_price">${wood.price()}</span> <img src=${woodImg} /> Vender</button>
+            <button id="sell_food" title="Comida"><span id="sell_food_price">${food.price()}</span> <img src=${foodImg} /> Vender</button>
+            <button id="sell_stone" title="Pedra"><span id="sell_stone_price">${stone.price()}</span> <img src=${stoneImg} /> Vender</button>
           </div>
 
           <div class="buy">
-            <button id="wood" title="Madeira"><span>${wood_exchange}</span> <img src=${woodImg} /> Comprar</button>
-            <button id="food" title="Comida"><span>${food_exchange}</span> <img src=${foodImg} /> Comprar</button>
-            <button id="stone" title="Pedra"><span>${stone_exchange}</span> <img src=${stoneImg} /> Comprar </button>
+            <button id="buy_wood" title="Madeira"><span id="buy_wood_price">${wood.price()}</span> <img src=${woodImg} /> Comprar</button>
+            <button id="buy_food" title="Comida"><span id="buy_food_price">${food.price()}</span> <img src=${foodImg} /> Comprar</button>
+            <button id="buy_stone" title="Pedra"><span id="buy_stone_price">${stone.price()}</span> <img src=${stoneImg} /> Comprar </button>
           </div>
         </div>
       </div>
     </div>
-  `
-  section__element.querySelector('.sell #wood').addEventListener('click', () => handleSell('wood'))
-  section__element.querySelector('.sell #food').addEventListener('click', () => handleSell('food'))
-  section__element.querySelector('.sell #stone').addEventListener('click', () => handleSell('stone'))
+  ` 
 
-  section__element.querySelector('.buy #wood').addEventListener('click', () => handleBuy('wood'))
-  section__element.querySelector('.buy #food').addEventListener('click', () => handleBuy('food'))
-  section__element.querySelector('.buy #stone').addEventListener('click', () => handleBuy('stone'))
+  section__element.querySelector('#sell_wood').addEventListener('click', () => { 
+    if (wood.quantity() >= wood.price()) {
+      wood.decrease()
+      gold.increase()
+    }
+  })
+  section__element.querySelector('#sell_food').addEventListener('click', () => { 
+    if (food.quantity() >= food.price()) {
+      food.decrease() 
+      gold.increase()
+    }  
+  })
+  section__element.querySelector('#sell_stone').addEventListener('click', () => { 
+    if (stone.quantity() >= stone.price()) {
+      stone.decrease()
+      gold.increase()
+    } 
+   })
+
+  section__element.querySelector('#buy_wood').addEventListener('click', () => { 
+    if (gold.quantity() >= 100) {
+      wood.increase()
+      gold.decrease()
+    }
+  })
+  section__element.querySelector('#buy_food').addEventListener('click', () => { 
+    if (gold.quantity() >= 100) {
+      food.increase()
+      gold.decrease()
+    }
+  })
+  section__element.querySelector('#buy_stone').addEventListener('click', () => {
+    if (gold.quantity() >= 100) {
+      stone.increase()
+      gold.decrease()
+    }
+  })
 
   return section__element
 }
 
-function updateExchange(resource) {
-  const section__element = document.getElementById('game_1')
+// function handlePlay() {
+//   const won = 0.0040
+//   const promptValue = prompt('Digite um valor em Reais para converter em Wons')
+//   const wonValue = promptValue * won
+//   const realValue = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(wonValue)
 
-  switch (resource) {
-    case 'wood':
-      section__element.querySelector('.exchange .sell #wood span').innerText = wood_exchange
-      section__element.querySelector('.exchange .buy #wood span').innerText = wood_exchange
-      break;
-  
-    case 'food':
-      section__element.querySelector('.exchange .sell #food span').innerText = food_exchange
-      section__element.querySelector('.exchange .buy #food span').innerText = food_exchange
-      break;
-
-    case 'stone':
-      section__element.querySelector('.exchange .sell #stone span').innerText = stone_exchange
-      section__element.querySelector('.exchange .buy #stone span').innerText = stone_exchange
-      break;
-  }
-}
-
-function updateResource(resource) {
-  const section__element = document.getElementById('game_1')
-
-  switch (resource) {
-    case 'wood':
-      section__element.querySelector('.resources #wood').innerText = wood
-      break;
-  
-    case 'food':
-      section__element.querySelector('.resources #food').innerText = food
-      break;
-
-    case 'stone':
-      section__element.querySelector('.resources #stone').innerText = stone
-      break;
-  } 
-}
-
-function handleSell(resource) {
-  switch (resource) {
-    case 'wood':
-      wood = wood - wood_exchange
-      gold.increase()
-      wood_exchange = wood_exchange - 2
-      updateResource('wood')
-      updateExchange('wood')
-      break;
-
-    case 'food':
-      food = food - food_exchange
-      gold.increase()
-      food_exchange = food_exchange - 2
-      updateResource('food')
-      updateExchange('food')
-      break;
-
-    case 'stone':
-      stone = stone - stone_exchange
-      gold.increase()
-      stone_exchange = stone_exchange - 2
-      updateResource('stone')
-      updateExchange('stone')
-      break;
-  
-    default:
-      break;
-  }
-}
-
-function handleBuy(resource) {
-  switch (resource) {
-    case 'wood':
-      wood = wood + wood_exchange
-      gold.decrease()
-      wood_exchange = wood_exchange + 2
-      updateResource('wood')
-      updateExchange('wood')
-      break;
-
-    case 'food':
-      food = food + food_exchange
-      gold.decrease()
-      food_exchange = food_exchange + 2
-      updateResource('food')
-      updateExchange('food')
-      break;
-
-    case 'stone':
-      stone = stone + stone_exchange
-      gold.decrease()
-      stone_exchange = stone_exchange + 2
-      updateResource('stone')
-      updateExchange('stone')
-      break;
-  
-    default:
-      break;
-  }
-}
-
-function handlePlay() {
-  const won = 0.0040
-  const promptValue = prompt('Digite um valor em Reais para converter em Wons')
-  const wonValue = promptValue * won
-  const realValue = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(wonValue)
-
-  alert('O valor em Reais é ' + realValue)
-}
+//   alert('O valor em Reais é ' + realValue)
+// }
