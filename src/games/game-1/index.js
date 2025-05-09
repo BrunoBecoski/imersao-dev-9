4d3df5318e29c4342426fc6b02e47176679dac4b
Bruno Becoski <brunobecoski@gmail.com>
2025-05-08 22:26:14 -0300

    Atualiza o componente Game 2

--- src/games/game-1/index.js
+++ src/games/game-1/index.js
@@ -304,6 +304,28 @@ function enableDisableExchange() {
   document.getElementById('sell_stone').disabled = stone.quantity() < stone.price()
 }
 
+
+
+
+
+// export function createGame1() {
+//   const section__element = document.createElement('section')
+  
+//   section__element.innerHTML = `
+//     <div class="container">
+//       <div class="content">
+//         <h2>Quem ganha, ganha em Wons!</h2>
+//         <p>Que tal converter esse valor</p>
+//         <button>Bora converter!</button>
+//       </div>
+//     </div>
+//   `
+
+//   section__element.querySelector('button').onclick = () => handlePlay()
+
+//   return section__element
+// }
+
 // function handlePlay() {
 //   const won = 0.0040
 //   const promptValue = prompt('Digite um valor em Reais para converter em Wons')
