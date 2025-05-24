18f2851eeb6e7a0107b3be808c65e44c52ae176b
Bruno Becoski <brunobecoski@gmail.com>
2025-05-23 23:01:20 -0300

    Atualiza o componente Game 3

--- src/games/game-3/index.js
+++ src/games/game-3/index.js
@@ -1,43 +1,64 @@
+import './styles.css'
+
 export function createGame3() {
   const section__element = document.createElement('section')
-  
+
   section__element.innerHTML = `
-    <div class="container">
-      <div class="content">
+    <div id="game_3">
+      <div class="header">
         <h2>Pra ganhar, é preciso coragem!</h2>
-        <p>Até mesmo para pisar em um chão feito de vidro</p>
-        <button>Bora jogar!</button>
       </div>
+
     </div>
   `
-
-  section__element.querySelector('button').onclick = () => handlePlay()
 
   return section__element
 }
 
-function handlePlay() {
-  let won = true
 
-  for(let round = 1; round <= 3; round++) {
-    const playerChoice = prompt(`Nível ${round}, escolha um vidro entre (1, 2, 3)?` )
-    const brokenFloor = Math.floor(Math.random() * 3) + 1
+
+
+
+// export function createGame3() {
+//   const section__element = document.createElement('section')
+  
+//   section__element.innerHTML = `
+//     <div class="container">
+//       <div class="content">
+//         <h2>Pra ganhar, é preciso coragem!</h2>
+//         <p>Até mesmo para pisar em um chão feito de vidro</p>
+//         <button>Bora jogar!</button>
+//       </div>
+//     </div>
+//   `
+
+//   section__element.querySelector('button').onclick = () => handlePlay()
+
+//   return section__element
+// }
+
+// function handlePlay() {
+//   let won = true
+
+//   for(let round = 1; round <= 3; round++) {
+//     const playerChoice = prompt(`Nível ${round}, escolha um vidro entre (1, 2, 3)?` )
+//     const brokenFloor = Math.floor(Math.random() * 3) + 1
    
-    if (['1', '2', '3'].includes(playerChoice)) {
-      if (playerChoice == brokenFloor) {
-        alert('Vidro quebrou! Acabou o jogo para você.')
-        won = false
-        break
-      } else {
-        alert(`Passou! Piso quebrado estava na ponte: ${brokenFloor}.`)
-      }
-    } else {
-      round--
-      alert('Escolha inválida')
-    }
-  }
+//     if (['1', '2', '3'].includes(playerChoice)) {
+//       if (playerChoice == brokenFloor) {
+//         alert('Vidro quebrou! Acabou o jogo para você.')
+//         won = false
+//         break
+//       } else {
+//         alert(`Passou! Piso quebrado estava na ponte: ${brokenFloor}.`)
+//       }
+//     } else {
+//       round--
+//       alert('Escolha inválida')
+//     }
+//   }
 
-  if (won == true) {
-    alert('Parabéns você venceu!')    
-  }
-}
+//   if (won == true) {
+//     alert('Parabéns você venceu!')    
+//   }
+// }
